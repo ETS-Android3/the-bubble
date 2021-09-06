0d516aeec6ce0d3e9fb292dc0642d94f65196b6c
tyichye <tal.19595@gmail.com>
2021-09-06 15:07:16 +0300

    fixing bug in background worker class

--- app/src/main/java/huji/postpc/y2021/tal/yichye/thebubble/BackgroundLocationWorker.java
+++ app/src/main/java/huji/postpc/y2021/tal/yichye/thebubble/BackgroundLocationWorker.java
@@ -1,9 +1,17 @@
 package huji.postpc.y2021.tal.yichye.thebubble;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
+import androidx.work.WorkerParameters;
 
 public class BackgroundLocationWorker extends Worker {
+
+	public BackgroundLocationWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+		super(context, workerParams);
+	}
+
 	@NonNull
 	@Override
 	public Result doWork() {
